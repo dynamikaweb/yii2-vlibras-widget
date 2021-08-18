c6bf65da1782a67b708e9aa9478b1a3b0d0edfa5
RodrigoDornelles <rodrigo@dornelles.me>
2021-08-18 11:28:51 -0300

    hotfix missing '-widget' in filepath renderFile

--- src/Vlibras.php
+++ src/Vlibras.php
@@ -11,8 +11,8 @@ class Vlibras extends \yii\base\Widget
      */
     public function run()
     {
-        return $this->view->registerJS(strtr($this->renderFile('@vendor/dynamikaweb/yii2-vlibras/assets/script.js'), [
-            '{main.html}' => $this->renderFile('@vendor/dynamikaweb/yii2-vlibras/assets/main.html')
+        return $this->view->registerJS(strtr($this->renderFile('@vendor/dynamikaweb/yii2-vlibras-widget/assets/script.js'), [
+            '{main.html}' => $this->renderFile('@vendor/dynamikaweb/yii2-vlibras-widget/assets/main.html')
         ]),
             $this->view::POS_END
         );
